53ff4e29fd398de1085556155b841fdb044a47ae
mahardikakdenie <kucingemas744@gmail.com>
2025-09-07 22:27:15 +0700

    feat:saparate logic UI and bussines between slug page at users page

--- pages/users/[slug].vue
+++ pages/users/[slug].vue
@@ -1,67 +1,14 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'auth' // ← nama file tanpa ekstensi: 'auth'
+});
+const route = useRoute();
+const isUserListPage = computed(() => route.params.slug === 'user-lists');
+</script>
+
+
 <template>
     <div>
-        <UsersSummary />
-        
-        <div class="mt-4">
-            <div class="bg-white shadow p-4 rounded-md">
-                <UiTable :headers="headerTables" :datas="dataTables" />
-            </div>
-        </div>
+        <UsersList v-if="isUserListPage" />
     </div>
 </template>
-
-<script lang="ts" setup>
-const route = useRoute();
-const dataTables = computed(() => {
-    let datas: any[] = [];
-
-    const slug = route.params.slug;
-    if (slug.includes('user-lists')) {
-        datas = [
-            {
-                name: 'Superadmin',
-                email: 'superadmin@mail.com',
-                status: 'Active',
-                roles: 'Superadmin',
-                actions: 'Edit | Delete'
-            }
-        ];
-    }
-    
-
-    return datas;
-});
-
-const headerTables = computed(() => {
-    let datas: any[] | undefined = undefined;
-
-    const slug = route.params.slug;
-
-    if (slug.includes('user-lists')) {
-        datas = [
-            {
-                name: 'Name',
-                key: 'name',
-            },
-            {
-                name: 'Email',
-                key: 'email',
-            },
-            {
-                name: 'Status',
-                key: 'status',
-            },
-            {
-                name: 'Roles',
-                key: 'roles',
-            },
-            {
-                name: 'Actions',
-                key: 'actions',
-            }
-        ];
-    }
-
-    return datas;
-})
-</script>
