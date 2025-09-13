f21725e117bfc4d67f7e72fdb496f6c050f9f450
mahardikakdenie <kucingemas744@gmail.com>
2025-09-13 23:39:41 +0700

    feat:product-categories endpoint Backend

---
+++ server/api/products/category/index.get.ts
@@ -0,0 +1,22 @@
+import { CommonResponse } from "~/types/common.types";
+import auth from "../../middleware/auth";
+
+export default defineEventHandler(async (event) => {
+    const config = useRuntimeConfig();
+    const masterEndpoint = 'product-categories';
+    try {
+        auth(event);
+        const masterDatas = await $fetch<CommonResponse<any[]>[]>(`${config.public.apiBaseUrl}?name=${masterEndpoint}`);
+
+        const categories = masterDatas.find(data => data.name === masterEndpoint);
+
+        return {
+            meta: {
+                status: true,
+            },
+            data: categories,
+        }
+    } catch (error) {
+        throw createError({statusCode: 500, message: 'Internal Server Error'});
+    }
+});
