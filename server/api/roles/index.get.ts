b03a60f268c7b168b9f1063693706633c07cd248
mahardikakdenie <kucingemas744@gmail.com>
2025-09-14 15:14:52 +0700

    feat:role data mapping added relation data

--- server/api/roles/index.get.ts
+++ server/api/roles/index.get.ts
@@ -4,19 +4,23 @@ import auth from "../middleware/auth";
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
     const endpoint = 'roles';
-    const urlEndpoint = `${config.public.apiBaseUrl}?name=${endpoint}`;
+    const urlEndpoint = `${config.public.apiBaseUrl}`;
 
     try {
         await auth(event);
 
         const masterData = await $fetch<CommonResponse<any[]>[]>(urlEndpoint);
         const roleData = masterData.find(data => data.name === endpoint);
+        const userData = masterData.find(data => data?.name === 'users');
 
         return {
             meta: {
                 status: 200,
             },
-            data: roleData?.datas,
+            data: roleData?.datas.map((role) => ({
+                ...role,
+                user_created: userData?.datas.find(user => user.id === role.created_by),
+            })),
         }
     } catch (error) {
         throw createError({ statusCode: 500 });
