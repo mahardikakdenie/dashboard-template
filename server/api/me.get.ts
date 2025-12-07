2c3d8ffa57a4cb21c41d70ab745955b058034830
mahardikakdenie <kucingemas744@gmail.com>
2025-12-07 22:58:20 +0700

    refactor(me): update logic beacuse update new endpoint

--- server/api/me.get.ts
+++ server/api/me.get.ts
@@ -13,45 +13,53 @@ export default defineEventHandler(
 	}> => {
 		const config = useRuntimeConfig();
 		const table = 'users';
-		const endpoint = `${config.public.apiBaseUrl}`;
-		
-		try {
-			await auth(event);
-			const payloadUser = event.context.user;
-			const response = await $fetch<CommonResponse<User[]>[]>(endpoint);
+		const endpoint = `${config.public.apiBaseUrl}/users/me`;
 
-			const users = response.find((res) => res.name === table);
-			const role = response.find((res) => res.name === 'roles');
-			const companies = response.find((res) => res.name === 'companies');
+		// try {
+		await auth(event);
+		const token = event.context.token;
+		const response = await $fetch<any>(endpoint, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'Content-Type': 'application/json',
+			},
+		});
 
-			const currentUser = users?.datas?.find(
-				(curr) => curr.id === payloadUser.id
-			);
-			const currRole =
-				role?.datas?.find(
-					(currRole) => currRole.id === currentUser?.roleId
-				) ?? role?.datas?.find((r) => r.name === 'superadmin');
-			const user = {
-				...currentUser,
-				password: undefined,
-				role: {
-					id: currRole?.id,
-					name: currRole?.name,
-				},
-				company:
-					companies?.datas?.find(
-						(currComp: Company) =>
-							currComp?.id === currentUser?.companyId
-					) ?? null,
-			};
+		// const users = response.find((res) => res.name === table);
+		// const role = response.find((res) => res.name === 'roles');
+		// const companies = response.find((res) => res.name === 'companies');
 
-			return {
-				success: true,
-				message: `success`,
-				data: user as User,
-			};
-		} catch (error) {
-			throw createError({ statusCode: 500 });
-		}
+		// const currentUser = users?.datas?.find(
+		// 	(curr) => curr.id === payloadUser.id
+		// );
+		// const currRole =
+		// 	role?.datas?.find(
+		// 		(currRole) => currRole.id === currentUser?.roleId
+		// 	) ?? role?.datas?.find((r) => r.name === 'superadmin');
+		// const user = {
+		// 	...currentUser,
+		// 	password: undefined,
+		// 	role: {
+		// 		id: currRole?.id,
+		// 		name: currRole?.name,
+		// 	},
+		// 	company:
+		// 		companies?.datas?.find(
+		// 			(currComp: Company) =>
+		// 				currComp?.id === currentUser?.companyId
+		// 		) ?? null,
+		// };
+
+		return {
+			success: true,
+			message: `success`,
+			data: response.data.data,
+		};
+
+
+		// } catch (error) {
+		// 	throw createError({ statusCode: 500 });
+		// }
 	}
 );
