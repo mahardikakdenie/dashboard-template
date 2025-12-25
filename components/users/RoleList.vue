a69ea3e62c3050e2b019f7991f193709ba9e6a00
mahardikakdenie <kucingemas744@gmail.com>
2025-12-26 04:31:37 +0700

    feat(role): list of data roles implementation with fetching data API

--- components/users/RoleList.vue
+++ components/users/RoleList.vue
@@ -1,25 +1,27 @@
 <script lang="ts" setup>
 import type { Role } from '~/types/user.type';
 import type { TableColumn } from '../ui/Table.vue';
+import type { META } from '~/types/common.types';
+import { useUserStore } from '~/store/users';
 const token = useCookie('auth_token');
 
 const headers = ref<TableColumn[]>([
-    {
-        key: "name",
-        label: "Name"
-    },
-    {
-        key: "created_by",
-        label: "Created By",
-        type: "text",
-    },
-    {
-        key: "created_at",
-        label: "Created At",
-        format: 'DD MMMM YYYY',
-        type: "date",
-    },
-    {
+	{
+		key: 'name',
+		label: 'Name',
+	},
+	{
+		key: 'created_by',
+		label: 'Created By',
+		type: 'text',
+	},
+	{
+		key: 'created_at',
+		label: 'Created At',
+		format: 'DD MMMM YYYY',
+		type: 'date',
+	},
+	{
 		key: 'actions',
 		label: 'Actions',
 		type: 'actions',
@@ -29,35 +31,45 @@ const headers = ref<TableColumn[]>([
 
 const datas = ref<Role[]>([]);
 const isLoading = ref<boolean>(true);
-const getDataRoles = async () => {
-    try {
-        const response = await $fetch("/api/roles", {
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-            }
-        });
+const perPage = ref<number>(10);
+const search = ref<string>('');
+const userStore = useUserStore();
+const getDataRoles = async (currentPage: number = 1) => {
+	try {
+        const token = userStore.token;
+		const response = await $fetch<{meta: META, data: any}>('/api/roles', {
+            params: {
+                page: currentPage,
+                limit: perPage.value,
+                search: search.value,
+            },
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
 
-        datas.value = response.data ?? [];
-        isLoading.value = false;
-        
-    } catch (error) {
-        console.log("error fetched role data : ", error);
-    }
+		datas.value = response.data ?? [];
+		isLoading.value = false;
+	} catch (error) {
+		console.log('error fetched role data : ', error);
+	}
 };
 
 onMounted(() => {
-   getDataRoles(); 
+	getDataRoles();
 });
 </script>
 
 <template>
-    <div class="mt-5">
-        <div class="bg-white shadow rounded-2xl p-5">
-            <UsersSummary />
-
-            <div class="mt-5">
-                <UiTable :headers="headers" :datas="datas" :is-loading="isLoading" />
-            </div>
-        </div>
-    </div>
+	<div class="mt-5">
+		<UsersSummary />
+		<div class="bg-white shadow rounded-2xl p-5 mt-4">
+			<div class="mt-5">
+				<UiTable
+					:headers="headers"
+					:datas="datas"
+					:is-loading="isLoading" />
+			</div>
+		</div>
+	</div>
 </template>
