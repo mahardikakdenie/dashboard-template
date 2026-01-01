47b6b2f52bd0896e616a0aa8e737f31e43132f20
mahardikakdenie <kucingemas744@gmail.com>
2026-01-02 05:23:02 +0700

    feat(home): create new logic for trees structure of companies pages

--- pages/home/[slug].vue
+++ pages/home/[slug].vue
@@ -1,108 +1,15 @@
 <template>
-    <div>
-        <div class="mt-4">
-			<div class="grid grid-cols-4 gap-4">
-				<div v-for="(summary, i) in summaries" :key="i">
-					<SummaryBox :summary="summary" />
-				</div>
+	<div>
+		<div>
+			<HomeCompaniesList v-if="slug === 'companies'" />
+			<div v-else>
+				halo
 			</div>
 		</div>
-
-        <div class="mt-4 shadow bg-white p-4 rounded-md">
-            <UiTable :datas="datas" />
-        </div>
-    </div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import buildingIcon from '~/components/icons/building.vue';
-import carouselVerticalIcon from '~/components/icons/carousel-vertical.vue';
-import chalkboardIcon from '~/components/icons/chalkboard.vue';
-import bussinesPlanIcon from '~/components/icons/bussines-plan.vue';
-import type { Summary } from '../index.vue';
-
 const route = useRoute();
-const title = computed((): string => {
-    if (typeof route?.params?.slug === 'object') {
-        return route?.params?.slug[0].split('-').join(' ');
-    } else {
-        return route?.params?.slug.replace('-', ' ');
-    }
-
-    return '';
-});
-
-
-const datas = ref<any>([
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-	{
-		name: 'PT Friendsure Indonesia',
-		email: 'office@teman.id',
-		url: 'https://github.com/mahardikakdenie',
-		status: 'Active',
-	},
-]);
-
-const summaries = ref<Summary[]>([
-	{
-		name: 'Total Companies',
-		value: 5672,
-		percentage: 20,
-		icon: markRaw(buildingIcon),
-	},
-	{
-		name: 'Active Companies',
-		value: 4576,
-		percentage: 20,
-		icon: markRaw(carouselVerticalIcon),
-	},
-	{
-		name: 'Total Subscribers',
-		value: 3696,
-		percentage: 20,
-		icon: markRaw(chalkboardIcon),
-	},
-	{
-		name: 'Total Earning',
-		value: 8987858,
-		percentage: 40,
-		icon: markRaw(bussinesPlanIcon),
-	},
-]);
-
+const slug = computed(() => route?.params?.slug ?? '');
 </script>
