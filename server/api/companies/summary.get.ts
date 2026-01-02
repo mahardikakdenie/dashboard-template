6698d6867c288d23b2aed1335477d3a329e941a8
mahardikakdenie <kucingemas744@gmail.com>
2026-01-02 09:24:56 +0700

    feat(summary-service): company new endpoint nuxt service for company statistic

--- server/api/companies/summary.get.ts
+++ server/api/companies/summary.get.ts
@@ -1,24 +1,27 @@
-import { CommonResponse } from "~/types/common.types";
+import { CommonResponse, ResponseApi } from "~/types/common.types";
 import auth from "../middleware/auth";
 import { Company } from "~/types/company.types";
 import queryNewData from "~/utils/queryNewData";
 
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
-    const endpoint = `${config.public.apiBaseUrl}`;
+    const endpoint = `${config.public.apiBaseUrl}/companies/stats`;
     try {
         await auth(event);
-
-        const response = await $fetch<CommonResponse<any>[]>(endpoint);
-        
-        const companies = response.find(resp => resp.name === 'companies');
+        const token = event.context.token;
+        const response = await $fetch<ResponseApi<{
+            all: number;
+        }>>(endpoint, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
 
         return {
-            totalCompany: companies?.datas.length,
-            active: companies?.datas.filter((company: Company) => company.status === 'active').length,
-            inactive: companies?.datas.filter((company: Company) => company.status === 'inactive').length,
-            pending: companies?.datas.filter((company: Company) => company.status === 'pending').length,
-            new: companies?.datas.filter((company: Company) => queryNewData(company.created_at)).length,
+            data: response.data,
+            meta: response.meta,
         }
     } catch (error) {
         throw createError({statusCode: 500});
